0bb547f71f36219d50b7cdcab03676ec60eda80c
haizei <339024hu@163.com>
2026-07-28 08:37:46 +0800

    Initial Commit

---
+++ foxgita/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  foxgita
+//
+//  Created by huguangyao on 2026/7/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ foxgita/foxgitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  foxgitaApp.swift
+//  foxgita
+//
+//  Created by huguangyao on 2026/7/28.
+//
+
+import SwiftUI
+
+@main
+struct foxgitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
